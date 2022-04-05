e6e56157c0deb9a0a9e6fdbfc9874562529bf953
facuV23 <68666415+facuV23@users.noreply.github.com>
2022-04-05 09:59:22 -0300

    Feature/101/contenido (#1)
    * agg datos al txt
    
    * guarde txt y main

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void)
+{
+    char buf[100];
+    FILE *fd;
+
+    cout<<"========= Inicio del programa ========"<<endl;
+    
+    fd= fopen("texto.txt","r+t");
+    fread(buf,100,1,fd);
+    printf("%s",buf);
+    fclose(fd);
+
+
+
+
+
+}
